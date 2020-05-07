d75dd5af1ace1315f59e8e0906b17537f6b87830
Nataliariosrod <natriorod@gmail.com>
2020-05-07 14:22:32 +0200

    Version 1.2

---
+++ src/Library/LibraryMain.java
@@ -0,0 +1,19 @@
+/*
+ * 
+ */
+package Library;
+
+/**
+ *
+ * @author Natalia Rios
+ */
+public class LibraryMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+    
+}
